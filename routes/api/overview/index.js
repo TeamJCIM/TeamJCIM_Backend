0672a4630c6334c4413c5ad349afecc295c179ef
soyeon <no02031@naver.com>
2022-03-09 19:16:33 +0900

    수정:overview, lookup_elec params로 변경

--- routes/api/overview/index.js
+++ routes/api/overview/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/mainoverview', require('./mainPageOverview.js'));
+router.use('/', require('./mainPageOverview.js'));
 
 module.exports = router;
 
