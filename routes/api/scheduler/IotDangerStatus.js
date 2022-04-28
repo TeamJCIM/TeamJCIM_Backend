98fb15ff38ddd5157e0e1d55e84d0de474fec38c
bohyeon <980926a@gmail.com>
2022-04-28 19:28:59 +0900

    충돌 해결

---
+++ routes/api/scheduler/IotDangerStatus.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const defaultRes = require('../../../module/utils/utils');
+const resMessage = require('../../../module/utils/responseMessage');
+const statusCode = require("../../../module/utils/statusCode");
+const db = require('../../../module/pool');
+
+
+
+module.exports = router;
