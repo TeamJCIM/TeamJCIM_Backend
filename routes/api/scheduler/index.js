415d16546a76bccf10305c2cc5df4df7e82b782e
soyeon <no02031@naver.com>
2022-04-28 19:44:22 +0900

    수정:테이블 변경

--- routes/api/scheduler/index.js
+++ routes/api/scheduler/index.js
@@ -3,5 +3,6 @@ var router = express.Router();
 
 router.use('/IotDangerStatus', require('./IotDangerStatus.js'));
 router.use('/IotStatusSchduler', require('./IotStatusSchduler.js'));
+router.use('/IotDangerScheduler', require('./IotDangerScheduler.js'));
 
 module.exports = router;
