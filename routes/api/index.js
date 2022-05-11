093df30884c58fbc6a4be4cef7fd3a55f48432d3
bohyeon <980926a@gmail.com>
2022-05-11 12:51:15 +0900

    수정 : 스케줄러 일부분 주석제거

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,6 +6,6 @@ router.use('/overview', require('./overview/index'));
 router.use('/lookup_elec', require('./lookup_elec/index'));
 router.use('/safety', require('./safety/index'));
 router.use('/predict', require('./predict/index'));
-// router.use('/scheduler', require('./scheduler/index'));
+router.use('/scheduler', require('./scheduler/index'));
 
 module.exports = router;
--- routes/api/scheduler/index.js
+++ routes/api/scheduler/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/IotDangerStatus', require('./IotDangerStatus.js'));
-router.use('/IotStatusSchduler', require('./IotStatusSchduler.js'));
-router.use('/IotDangerScheduler', require('./IotDangerScheduler.js'));
+// router.use('/IotStatusSchduler', require('./IotStatusSchduler.js'));
+// router.use('/IotDangerScheduler', require('./IotDangerScheduler.js'));
 
 
 module.exports = router;
